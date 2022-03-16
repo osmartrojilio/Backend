703c1e94b1fc117239c3545c9e8bae557cdac87e
Osmar trojilio <80086189+osmartrojilio@users.noreply.github.com>
2022-03-16 01:47:00 -0300

    rotas prontas index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/database");
+const res = require("express/lib/response");
 
 const app = express();
 
@@ -43,60 +44,119 @@ app.get("/produtos/cardapio", function (request, response) {
             response.status(500).send(err);
 
         } else {
-            return response.status(200).json(result);
+            return response.status(201).json(result);
         }
     });
 });
 
 app.post("/pedidos", function (request, response) {
 
-    /* let ssql = "select c.descricao as categoria, p.* ";
-     ssql += "from produto p ";
-     ssql += "join produto_categoria c on (c.id_categoria = p.id_categoria) ";
-     ssql += "order by c.ordem";
- 
-     db.query(ssql, function (err, result) {
-         if (err) {
-             response.status(500).send(err);
- 
-         } else {
-             return response.status(200).json(result);
-         }
-     });*/
+    db.beginTransaction(function (err) {
+
+        let ssql = "insert into pedido(id_usuario, dt_pedido, vl_subtotal, vl_entrega , vl_total, status) ";
+        ssql += "values(?, current_timestamp(), ?, ?, ?, 'A')";
+
+        db.query(ssql, [request.body.id_usuario, request.body.vl_subtotal, request.body.vl_entrega,
+        request.body.vl_total], function (err, result) {
+
+            if (err) {
+                db.rollback();
+                response.status(500).json(err);
+            } else {
+                var id_pedido = result.insertId;
+
+                if (id_pedido > 0) {
+                    const itens = request.body.itens;
+                    var values = [];
+
+                    for (var i = 0; i < itens.length; i++) {
+                        values.push([id_pedido, itens[i].id_produto, itens[i].qtd, itens[i].vl_unitario, itens[i].vl_total]);
+                    }
+
+                    ssql = "insert into pedido_item(id_pedido, id_produto, qtd, vl_unitario, vl_total)";
+                    ssql += "values ?";
+
+                    db.query(ssql, [values], function (err, result) {
+                        if (err) {
+                            db.rollback();
+                            response.status(500).json(err);
+                        } else {
+                            db.commit();
+                            response.status(201).json({ id_pedido: id_pedido });
+                        }
+                    });
+                }
+            }
+
+        });
+    });
+
 });
+
+
 
 app.get("/pedidos", function (request, response) {
 
-    /* let ssql = "select c.descricao as categoria, p.* ";
-     ssql += "from produto p ";
-     ssql += "join produto_categoria c on (c.id_categoria = p.id_categoria) ";
-     ssql += "order by c.ordem";
- 
-     db.query(ssql, function (err, result) {
-         if (err) {
-             response.status(500).send(err);
- 
-         } else {
-             return response.status(200).json(result);
-         }
-     });*/
+    let ssql = "select p.id_pedido, p.status, date_format(p.dt_pedido, '%d/%m/%y %H:%i:%s') as dt_pedido, ";
+    ssql += "p.vl_total, count(*) as qtd_item ";
+    ssql += "from pedido p ";
+    ssql += "join pedido_item i on (i.id_pedido = p.id_pedido) ";
+    ssql += "group by p.id_pedido, p.status, p.dt_pedido, p.vl_total";
+
+    db.query(ssql, function (err, result) {
+        if (err) {
+            response.status(500).send(err);
+
+        } else {
+            return response.status(200).json(result);
+        }
+    });
 });
+
+app.get("/pedidos/itens", function (request, response) {
+
+    /*let ssql = "select c.descricao as categoria, p.* ";
+    ssql += "from produto p ";
+    ssql += "join produto_categoria c on (c.id_categoria = p.id_categoria) ";
+    ssql += "order by c.ordem";
+
+    db.query(ssql, function (err, result) {
+        if (err) {
+            response.status(500).send(err);
+
+        } else {
+            return response.status(200).json(result);
+        }
+    });*/
+});
+
+app.put("/pedidos/status/:id_pedido", function (request, response) {
+
+    let ssql = "update pedido set status = ? where id_pedido = ? ";
+
+    db.query(ssql, [request.body.status, request.params.id_pedido], function (err, result) {
+        if (err) {
+            return response.status(500).send(err);
+        } else {
+            return response.status(200).json({ id_pedido: request.params.id_pedido });
+        }
+    });
+});
+
+
 
 app.get("/configs", function (request, response) {
 
-    /* let ssql = "select c.descricao as categoria, p.* ";
-     ssql += "from produto p ";
-     ssql += "join produto_categoria c on (c.id_categoria = p.id_categoria) ";
-     ssql += "order by c.ordem";
- 
-     db.query(ssql, function (err, result) {
-         if (err) {
-             response.status(500).send(err);
- 
-         } else {
-             return response.status(200).json(result);
-         }
-     });*/
+    let ssql = "select * from config ";
+
+    db.query(ssql, function (err, result) {
+        if (err) {
+            response.status(500).send(err);
+
+        } else {
+            return response.status(200).json(result[0]);
+        }
+    });
 });
 
 
